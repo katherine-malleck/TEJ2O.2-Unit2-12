5b66fef35f77e935390d7b97db0796bde37eb56e
katherine <katherine.malleck@stu.ocsb.ca>
2026-03-26 09:06:22 -0400

    neopixels are codes and ya pretty cool

--- main.ts
+++ main.ts
@@ -1,8 +1,34 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
+ * Created by: Katherine
+ * Created on: Mar 2026
  * This program ...
 */
 
-basic.showString('Hello, World!')
+let distance = 0
+let strip = neopixel.create(DigitalPin.P1, 8, NeoPixelMode.RGB)
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    // Measureing the distance...
+    distance = sonar.ping(
+        DigitalPin.P0,
+        DigitalPin.P0,
+        PingUnit.Centimeters
+    )
+
+    // ify elesy stuffff
+    if (distance < 10) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    } else {
+        // Green if 10cm or further
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    }
+})
+
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
